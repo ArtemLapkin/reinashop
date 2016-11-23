2b5c98bd388944d8afdff395d0599fe884430368
Artem Lapkin <lapkin.artem@gmail.com>
2016-11-23 11:30:26 +0200

    parralax 2 sections, clothes pics added

--- js/main.js
+++ js/main.js
@@ -1,9 +1,34 @@
 (function($){
-  // Init Skrollr
+  // init Skrollr
   var s = skrollr.init({
     render: function(data) {
-      //Debugging - Log the current scroll position.
-      //console.log(data.curTop);
+
     }
   });
+
+
+
+  $(window).scroll(function() {
+    var wScroll = $(this).scrollTop();
+
+    // logo animation
+    $('.js-logo').css({
+      'transform' : 'translate(0px, '+ wScroll/2  +'%)'
+    });
+
+
+    // clothes elements
+    if(wScroll > $('.clothes-pics').offset().top - ($(window).height() / 1.2)) {
+
+      $('.clothes-pics figure').each(function(i){
+
+        setTimeout(function(){
+          $('.clothes-pics figure').eq(i).addClass('is-showing');
+        }, (700 * (Math.exp(i * 0.14))) - 700);
+      });
+
+    }
+
+  })
+
 })(jQuery);
